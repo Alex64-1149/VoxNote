1d5fa6f42d698ae2d9c90e6467dd2fce4f361cb8
Jeremie Lestage <jeremielestage44@gmail.com>
2024-04-05 12:43:45 -0400

    peut enregistrer la voix mais pas capable de mettre de reponse

--- user/views.py
+++ user/views.py
@@ -1,14 +1,9 @@
 
 from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import UserRegisterForm
-from django.core.mail import send_mail
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import get_template
-from django.template import Context
+
 
 
 ########### register here ##################################### 
@@ -17,15 +12,10 @@ def creerCompte(request):
 		form = UserRegisterForm(request.POST)
 		if form.is_valid():
 			form.save()
-			username = form.cleaned_data.get('username')
-			email = form.cleaned_data.get('email')
-			######################### mail system #################################### 
-			
-			################################################################## 
 			return redirect('voxnote-login')
 	else:
 		form = UserRegisterForm()
-	return render(request, 'user/creerCompte.html', {'form': form, 'title':'register here'})
+	return render(request, 'user/creerCompte.html', {'form': form, 'title':'Créer un compte'})
 
 
 
@@ -33,15 +23,11 @@ def creerCompte(request):
 ################ login forms################################################### 
 def user_login(request):
 	if request.method == 'POST':
-
-		# AuthenticationForm_can_also_be_used__
-
 		username = request.POST['username']
 		password = request.POST['password']
 		user = authenticate(request, username = username, password = password)
 		if user is not None:
 			form = login(request,user)
-			messages.success(request, f' welcome {username} !!')
 			return redirect('voxnote-accueil')
 	form = AuthenticationForm()
-	return render(request, 'user/login.html', {'form':form, 'title':'log in'})
+	return render(request, 'user/login.html', {'form':form, 'title':'Se connecter'})
--- voxnote/views.py
+++ voxnote/views.py
@@ -4,8 +4,25 @@ from django.http import JsonResponse
 from pydub import AudioSegment
 import io
 
+import random
+import string
+
+def generate_random_string():
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for _ in range(4))
+
 def accueil(request):
-    return render(request,"voxnote/accueil.html")
+    recognized_text=None
+    if request.method == 'POST' and request.FILES.get('audio_file'):
+        audio_file = request.FILES['audio_file']
+        print(audio_file)
+        # Process the audio file
+        #recognized_text = recognize_speech(audio_file)
+        print('hello')
+        recognized_text=generate_random_string()
+        print(recognized_text)
+    return render(request,"voxnote/accueil.html",{'recognized_text': recognized_text})
+
 
 def mesNotes(request):
     return render(request,"voxnote/notes.html")
